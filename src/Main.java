d9543499b7479bbd4566085c1522ea45ec7a9f9f
127RSire <127rsire@frhsd.com>
2025-09-09 08:37:19 -0400

    good name, commit.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello my name is Paul");
+        System.out.print("Hello my name is Rishi");
 
 
     }
