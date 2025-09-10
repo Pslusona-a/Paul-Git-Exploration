69c3bd15850dce02a964c4cd3690802ce51f9937
127MSafran <127msafran@frhsd.com>
2025-09-10 08:52:34 -0400

    Add new print statement

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello my name is Paul");
+        System.out.println("Hello my name is Mason");
 
 
     }
